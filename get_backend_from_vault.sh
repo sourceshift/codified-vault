89e38f70e462ed98dc7032b4ccf7518641502cd7
arpan <arpan.rec@outlook.com>
2022-01-10 16:29:56 +0530

    S3_BACKEND_CONFIG

--- get_backend_from_vault.sh
+++ get_backend_from_vault.sh
@@ -9,6 +9,6 @@ if [[ -z "$VAULT_TOKEN" ]]; then
     exit 1
 fi
 
-S3_BACKEND_CONFIG=$(curl -s -X GET -H "X-Vault-Token: $VAULT_TOKEN" "$VAULT_ADDR/v1/secret/data/tfstate/s3/codified-vault" | jq .data.data)
+S3_BACKEND_CONFIG=$(curl -s -X GET -H "X-Vault-Token: $VAULT_TOKEN" "$VAULT_ADDR/v1/secret/data/terraform/backend/s3/codified-vault" | jq .data.data)
 
 echo '{"terraform":{"backend": {"s3": '"$S3_BACKEND_CONFIG"'}}}' | jq . > ./backend-config.tf.json
